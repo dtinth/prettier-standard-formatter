90e238ff031cc09bdda00a74dae81bae8bb433f1
Thai Pangsakulyanont <dtinth@spacet.me>
2017-02-26 18:49:32 +0700

    Update prettier config

--- cli.js
+++ cli.js
@@ -21,7 +21,7 @@ function run () {
   const recursive = require('recursive-readdir')
   const prettierStandard = require('./')
 
-  const DEFAULT_IGNORE_LIST = [ '.git', 'node_modules', '!*.js' ]
+  const DEFAULT_IGNORE_LIST = ['.git', 'node_modules', '!*.js']
 
   const format = path => {
     fs.readFile(path, 'utf-8', (err, sourceCode) => {
