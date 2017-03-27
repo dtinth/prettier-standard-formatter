eaea4728fc5aa28f93d21e4bcf05ad7b70522f6e
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2017-03-27 19:51:19 +1300

    Refactor

--- src/cli.js
+++ src/cli.js
@@ -1,69 +1,40 @@
 #!/usr/bin/env node
-'use strict'
-main()
+'use strict';
 
-function main () {
-  const resolveCwd = require('resolve-cwd')
-  const localCLI = resolveCwd('prettier-semistandard/cli')
+const R = require('ramda');
+const resolveCwd = require('resolve-cwd');
+const globby = require('globby');
+const meow = require('meow');
+const lib = require('./index.js');
 
-  if (localCLI && localCLI !== __filename) {
-    require(localCLI)
-    return
-  }
+const DEFAULT_IGNORE_LIST = ['.git', 'node_modules', '!*.js'];
 
-  run()
-}
+const localCliPath = resolveCwd('prettier-semistandard/cli');
 
-function run () {
-  const fs = require('fs')
-  const globby = require('globby')
-  const meow = require('meow')
-  const recursive = require('recursive-readdir')
-  const prettierSemistandard = require('./')
-
-  const DEFAULT_IGNORE_LIST = ['.git', 'node_modules', '!*.js']
-
-  const format = path => {
-    fs.readFile(path, 'utf-8', (err, sourceCode) => {
-      if (err) throw err
-      prettierSemistandard.format(sourceCode).then(output => {
-        fs.writeFile(path, output, 'utf-8', err => {
-          if (err) throw err
-          console.log(path)
-        })
-      })
-    })
-  }
-
-  const processPaths = paths => {
-    paths.forEach(path => {
-      if (!fs.lstatSync(path).isDirectory()) {
-        format(path)
-      } else {
-        recursive(path, DEFAULT_IGNORE_LIST, (err, files) => {
-          if (err) throw err
-          files.forEach(format)
-        })
-      }
-    })
-  }
-
-  const cli = meow(
-    `
-    Usage
-      $ prettier-semistandard [<file|glob> ...]
-
-    Examples
-      $ prettier-semistandard
-      $ prettier-semistandard index.js
-      $ prettier-semistandard foo.js bar.js
-      $ prettier-semistandard index.js src/**/*.js
+const cli = meow(
   `
-  )
+  Usage
+    $ prettier-semistandard [<file|glob> ...]
 
-  if (!cli.input.length) {
-    cli.showHelp(1)
-  }
+  Examples
+    $ prettier-semistandard
+    $ prettier-semistandard index.js
+    $ prettier-semistandard foo.js bar.js
+    $ prettier-semistandard index.js src/**/*.js
+`
+);
 
-  globby(cli.input).then(processPaths)
+if (localCliPath && localCliPath !== __filename) {
+  // Local copy already exists; load it instead
+  require(localCliPath);
+} else if (cli.input.length === 0) {
+  // No args used; show help
+  cli.showHelp(1);
+} else {
+  // Run CLI
+  R.composeP(
+    R.map(p => p.then(console.log)),
+    lib.formatPaths(DEFAULT_IGNORE_LIST),
+    globby
+  )(cli.input);
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,69 @@
-const prettier = require('prettier')
-const semistandard = require('semistandard')
+'use strict';
 
-exports.format = source => new Promise((resolve, reject) => {
-  const pretty = prettier.format(source, {
-    printWidth: 80,
-    tabWidth: 2,
-    parser: 'babylon',
-    singleQuote: true,
-    trailingComma: 'none',
-    bracketSpacing: true
-  })
-  semistandard.lintText(pretty, { fix: true }, (err, result) => {
-    if (err) {
-      return reject(err)
+const Promise = require('bluebird');
+const R = require('ramda');
+const fs = require('mz/fs');
+const prettier = require('prettier');
+const semistandard = require('semistandard');
+const recursiveReaddir = Promise.promisify(require('recursive-readdir'));
+
+// Run linter on a string
+// :: inCode -> ( Promise -> outCode )
+const lint = code => new Promise((resolve, reject) =>
+  // Standard-engine doesn't support promisify
+  semistandard.lintText(code, { fix: true }, (error, result) => {
+    if (error) {
+      return reject(error);
     }
-    const output = result.results[0].output
-    if (typeof output !== 'string') {
-      return reject(new Error('Expected a string back from linter'))
-    }
-    resolve(output)
-  })
-})
+    const output = result.results[0].output;
+    return resolve(output || code);
+  }));
+
+// Run prettier on a string
+// :: inCode -> outCode
+const pretty = code => prettier.format(code, {
+  printWidth: 80,
+  tabWidth: 2,
+  parser: 'babylon',
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true
+});
+
+// Run formatter on a string
+// :: inCode -> ( Promise -> outCode )
+const format = R.composeP(lint, pretty, Promise.resolve);
+
+// Run formatFile on a list of file & dir paths
+// :: ignoreGlobs -> filePaths -> ( Promise -> filePaths )
+const formatPaths = R.curry((ignore, paths) =>
+  R.composeP(
+    R.map(formatFile),
+    R.unnest,
+    Promise.all,
+    R.map(path =>
+      R.composeP(
+        isDir => isDir ? recursiveReaddir(path, ignore) : [path],
+        stat => stat.isDirectory(),
+        fs.lstat
+      )(path)),
+    Promise.resolve
+  )(paths));
+
+// Run formatter on a file, overwrite it, then return path
+// :: filePath -> ( Promise -> filePath )
+const formatFile = path =>
+  R.composeP(
+    R.always(path),
+    output => fs.writeFile(path, output),
+    format,
+    buffer => buffer.toString(),
+    fs.readFile
+  )(path);
+
+Object.assign(exports, {
+  pretty,
+  format,
+  formatFile,
+  formatPaths
+});
