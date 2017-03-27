8e85bf0dc0ce516c690b4e98dab1cd0305f3df24
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2017-03-27 19:44:49 +1300

    Update paths

--- example.js
+++
@@ -1,6 +0,0 @@
-const prettierSourceCode = require('fs').readFileSync(
-  require.resolve('prettier'),
-  'utf8'
-)
-const prettierSemistandard = require('./')
-prettierSemistandard.format(prettierSourceCode).then(console.log)
