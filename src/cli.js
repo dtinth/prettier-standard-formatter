2ae944492323242afdbe5c24b051db9a1e599532
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2017-03-28 21:50:03 +1300

    Add CLI tests

--- src/cli.js
+++ src/cli.js
@@ -22,21 +22,19 @@ const cli = meow(
 `
 );
 
-if (cli.input.length === 0) {
-  // No args used; show help
-  cli.showHelp(1);
-} else {
-  // Run CLI
-  stdin().then(data => {
-    if (data) {
-      // Process stdin
-      return R.composeP(s => process.stdout.write(s), lib.format);
-    }
-    // Process a list of globs
-    return R.composeP(
-      R.map(p => p.then(console.log)),
-      lib.formatPaths(DEFAULT_IGNORE_LIST),
-      globby
-    )(cli.input);
-  });
-}
+stdin().then(data => {
+  if (data) {
+    // Process stdin
+    return R.composeP(s => process.stdout.write(s), lib.format)(data);
+  }
+  if (cli.input.length === 0) {
+    // No args used; show help
+    return cli.showHelp(1);
+  }
+  // Process a list of globs
+  return R.composeP(
+    R.map(p => p.then(console.log)),
+    lib.formatPaths(DEFAULT_IGNORE_LIST),
+    globby
+  )(cli.input);
+});
--- test/cli.js
+++ test/cli.js
@@ -1,11 +1,44 @@
 const tape = require('tape');
+const childProcess = require('child_process');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
 tape.test('cli', test => {
   test.plan(3);
 
-  test.skip('Help should be shown if no args used');
+  test.equal(
+    (() => {
+      try {
+        childProcess.execSync('node ./src/cli.js');
+      } catch (e) {
+        const out = e.stdout.toString();
+        return out.indexOf('Usage\n') > 1;
+      }
+    })(),
+    true,
+    'Help should be shown if no args used'
+  );
 
-  test.skip('stdin should be processed if provided');
+  test.equal(
+    childProcess
+      .execSync('node ./src/cli.js', {
+        input: '\nconsole.log("123")\n\n',
+        stdio: 'pipe'
+      })
+      .toString(),
+    `console.log('123');\n`,
+    'stdin should be processed if provided'
+  );
 
-  test.skip('Globs should be processed if stdin not provided');
+  rimraf.sync('test/.temp/cli');
+  mkdirp.sync('test/.temp/cli');
+  fs.writeFileSync('test/.temp/cli/1', '\n    console.log("0")');
+  childProcess.execSync('node ./src/cli.js test/.temp/**/*');
+
+  test.equal(
+    fs.readFileSync('test/.temp/cli/1', 'utf8'),
+    `console.log('0');\n`,
+    'Globs should be processed if stdin not provided'
+  );
 });
